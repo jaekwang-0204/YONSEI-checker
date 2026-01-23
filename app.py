dd994a348c93b5ac5949462796c1e82c9fe9ffbd
jaekwang-0204 <jaekwang1164@gmail.com>
2026-01-23 17:39:37 +0900

    Update app.py

--- app.py
+++ app.py
@@ -124,7 +124,7 @@ def ocr_image_parsing(image_file, year, version, dept):
                 try:
                     credit = float(match.group(2))
                     if credit != 0: 
-                        if credit % 0.5 != 0 or credit < 0.5 or credit > 5.0 or credit < 0.5:
+                        if credit % 0.5 != 0 or credit < 0.5 or credit > 5.0 or credit <= 0.4:
                             continue
                 except: continue
                     
@@ -482,6 +482,7 @@ with tab2:
             st.dataframe(pd.DataFrame(final_courses), use_container_width=True)
     else:
         st.info("성적표 이미지를 업로드하고 분석 버튼을 눌러주세요.")
+
 
 
 
